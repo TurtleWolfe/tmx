b51d69419637c237f10daebfdfb6bc4e7b247fae
TurtleWolfe <7327259+TurtleWolfe@users.noreply.github.com>
2022-03-09 18:36:57 -0500

    Mu image card: faveList (#10)
    * started muImageCard
    
    * ImageList favorite heart breaks
    
    * ImageList favorite heart breaks
    
    * faveList for home page
    
    * wider gap and rounded corners
    
    * lorem picsum placeholders
    
    * ImageListItemBar moved to the bottom
    
    * lowered one rating for demo
    
    * pushing to Dbobe to add his call to the photo utility
    
    * FaveList with images from Google Maps API
    
    * title heading text color
    
    * FaveList reviewed code
    
    * renamed FaveList, added .env.local to .gitignore & respelled typo for variant masonry
    
    * quick fix to faveList
    
    * Unnecessary comments removed
    
    * Comments removed
    
    Co-authored-by: David Bobe <60373369+dbobe@users.noreply.github.com>
    Co-authored-by: David Nesterov-Rappoport <48499573+NullDefault@users.noreply.github.com>
    Co-authored-by: NullDefault <davisha999@gmail.com>

---
+++ src/components/FaveList.js
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import {
+  Container,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  IconButton,
+} from "@mui/material";
+import {
+  Favorite,
+  HeartBroken,
+  StarBorder,
+  StarRate,
+} from "@mui/icons-material";
+import itemData from "../assets/data/favoritesTestData.json";
+
+import PlacePhoto from "./utility/PlacePhoto";
+
+function srcset(image, width, height, rows = 1, cols = 1) {
+  return {
+    src: `${image}&w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
+    srcSet: `${image}&w=${width * cols}&h=${
+      height * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
+
+export default function CustomImageList() {
+  const [starred, setStarred] = useState(false);
+  return (
+    <Container>
+      <ImageList
+        sx={{
+          maxHeight: "80vh",
+          // Promote the list into its own layer in Chrome. This costs memory, but helps keeping high FPS.
+          transform: "translateZ(0)",
+          borderRadius: "0.5rem",
+        }}
+        rowHeight={200}
+        gap={10}
+      >
+        {itemData.map((item) => {
+          const cols = item.featured ? 2 : 1;
+          const rows = item.featured ? 2 : 1;
+
+          return (
+            <ImageListItem
+              key={item.place_id}
+              cols={cols}
+              rows={rows}
+              sx={{
+                overflow: "hidden",
+                borderRadius: "0.5rem",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                {...srcset(
+                  PlacePhoto(item.photos[0].photo_reference, 500),
+                  250,
+                  200,
+                  rows,
+                  cols
+                )}
+                alt={item.title}
+                loading="lazy"
+              />
+              <ImageListItemBar
+                sx={{
+                  background:
+                    "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
+                    "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+                  width: "100%",
+                  borderRadius: "0.5rem",
+                  color: "primary",
+                }}
+                title={item.name}
+                subtitle={item.formatted_address}
+                color="primary"
+                actionIcon={
+                  <IconButton
+                    color={starred ? "primary.main" : "error"}
+                    aria-label={`star ${item.title}`}
+                    onClick={() => {
+                      setStarred(!starred);
+                    }}
+                  >
+                    {starred ? <StarBorder /> : <StarRate />}
+                    {item.rating}
+                    {starred ? <HeartBroken /> : <Favorite />}
+                    {item.user_ratings_total}
+                  </IconButton>
+                }
+                actionPosition="left"
+              />
+            </ImageListItem>
+          );
+        })}
+      </ImageList>
+    </Container>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
-import svg_art from "../assets/coding_art.svg";
-import MuiCard from "../components/muiCard";
+import FaveList from "../components/FaveList";
+// import svg_art from "../assets/coding_art.svg";
+// import MuiCard from "../components/muiCard";
 
 export default function Home() {
   return (
@@ -13,17 +14,7 @@ export default function Home() {
         width: "100%",
       }}
     >
-      <h1>Home</h1>
-      {/* <img alt="funky illustration" src={svg_art} /> */}
-      {/* <Box sx={{ width: "50%" }}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-      </Box> */}
-      <MuiCard
-        mapImage={svg_art}
-        mapTitle="tMX"
-        mapDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam."
-        mapLink="https://www.google.com"
-      />
+      <FaveList />
     </Box>
   );
 }
